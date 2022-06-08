61967f35c346acf7a6239d2c30bab762b6541ad4
spinkymax <99490816+spinkymax@users.noreply.github.com>
2022-06-08 15:05:25 +0300

    Test

--- students_test.go
+++ students_test.go
@@ -86,14 +86,15 @@ func TestSwap(t *testing.T) {
 		v := tcase
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			per1 := v.A[v.i]
-			per2 := v.A[v.j]
 			v.A.Swap(v.i, v.j)
-			if v.A[v.i] != per2 || v.A[v.j] != per1 {
-				t.Errorf("[%s] expected: %v got: %v", name, per1, per2)
+			if v.A[0].firstName != "Dmitry" || v.A[1].firstName != "Vitaly" {
+				if v.A[0].lastName != "Tolkachev" || v.A[1].lastName != "Donikov" {
+					if v.A[0].birthDay != time.Date(1987, 11, 15, 0, 0, 0, 0, time.UTC) || v.A[1].birthDay != time.Date(1987, 05, 18, 0, 0, 0, 0, time.UTC) {
 
+						t.Errorf("[%s] expected: %v, got %v", name, v.A[0], v.A[1])
+					}
+				}
 			}
-
 		})
 
 	}
